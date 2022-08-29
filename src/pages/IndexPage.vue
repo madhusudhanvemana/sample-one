46d17cb29c32614c4df014c5d8d34d57d7b97492
Madhusudhan <madhuvemana1998@gmail.com>
2022-08-29 12:50:54 +0530

    index.vue

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page padding>
+  <p style="position; background:primary;">Grocery Store<br>
+Registration</p>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      task: [
+        {
+
+        }
+      ]
+    }
+  }
+}
+
+</script>
